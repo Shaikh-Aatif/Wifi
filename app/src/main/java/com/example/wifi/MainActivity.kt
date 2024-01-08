582d7b644e052e0442e3a0bc586acc9609c7f73a
Shaikh-Aatif <aatifshaikh523@gmail.com>
2024-01-08 12:41:23 +0530

    app hiding feature completed

--- app/src/main/java/com/example/wifi/MainActivity.kt
+++ app/src/main/java/com/example/wifi/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.wifi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val p = packageManager
+        val componentName = ComponentName(this, MainActivity::class.java)
+        p.setComponentEnabledSetting(
+            componentName,
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
 
-        //Todo: Add code here
 
-        //Todo new branch
     }
 }
